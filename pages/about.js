ed8f1323849de3dbd6a3ec001775e417198a7454
william hernandez <william@hernandez2.com>
2019-05-09 23:48:32 -0400

    update

--- pages/about.js
+++ pages/about.js
@@ -1,101 +1,174 @@
-
-import ClipboardJS from 'clipboard/dist/clipboard'
-import Layout from '../components/Layout'
+import ClipboardJS from "clipboard/dist/clipboard";
+import Layout from "../components/Layout";
 
 class About extends React.Component {
+  state = {
+    clipboard: false
+  };
+  activateLasers = () => {
+    new ClipboardJS(".clipboard");
+    this.setState({ clipboard: true });
+    setTimeout(() => {
+      this.setState({ clipboard: false });
+    }, 1000);
+  };
 
-    state = {
-        clipboard: false
-    }
-    activateLasers = () => {
-        new ClipboardJS('.clipboard');
-        this.setState({clipboard: true });
-        setTimeout(() => {
-            this.setState({clipboard: false });
-        }, 1000)
-    }
-
-    render(){
-    
-        return (
-            <section className="section">
-                <header className="bottom-highlight m-b-50">
-                <div class="columns">
-                    <div class="column is-three-quarters-widescreen">
-                    <div className="columns">
-                        <div className="column"><h1  className="is-main-title highlight">Nice to meet you, I’m Zoraida. </h1></div>
-                    </div>
-                    <div className="columns is-multiline section">
-                    <div className="column is-half">
-                        <h2 className="highlight is-sub-title is-bold">What I do</h2>
-                        <p>I define users goals, and develop identities to craft suitable user interfaces so that brands can grow.</p>
-                    </div>
-                    <div className="column is-half">
-                        <h2 className="highlight is-sub-title is-bold">My focus is to… </h2>
-                        <p>Design stories that empathize with your customers, transforming the way they see you, and hopefully changing the way you see them too.</p>
-                    </div>
-                    <div className="column is-half">
-                        <h2 className="highlight is-sub-title is-bold">Why I do it</h2>
-                        <p>I've done many things in my life, design just happens to be my vocation.  <span className="highlight">I want to keep challenging my self with awesome work that provides me the opportunity to positively affect people.</span></p>
-                    </div>
-                    <div className="column is-half">
-                        <h2 className="highlight is-sub-title is-bold">Little details</h2>
-                        <p>I am very enthusiastic about learning new things.  I'm a history buff, a video game geek, an aspiring peripatetic who has had the privilege to live in: Puerto Rico, Mexico, and currently: <span className="highlight">Dayton, Ohio</span>.</p>
-                    </div>
-                    </div>
-                    </div>
-                    <div class="column is-one-quarter-desktop is-hidden-movil zory-back">
-                    </div>
+  render() {
+    return (
+      <section className="section">
+        <header className="bottom-highlight m-b-50">
+          <div className="columns">
+            <div className="column">
+              <h1 className="is-main-title highlight">
+                Nice to meet you, I’m Zoraida.{" "}
+              </h1>
+            </div>
+          </div>
+          <div className="columns">
+            <div className="column ">
+              <div className="columns is-multiline section">
+                <div className="column is-full">
+                  <h2 className="highlight is-sub-title is-bold">
+                    My focus is to…{" "}
+                  </h2>
+                  <p>
+                    Design stories that empathize with your customers,
+                    transforming the way they see you, and hopefully changing
+                    the way you see them too.
+                  </p>
                 </div>
-                </header>
-                <section>
-                <div class="columns">
-                    <div class="column">
-                        <div className="columns">
-                            <div className="column"><h2  className="is-sub-title highlight is-bold has-text-centered">A little more about me: </h2></div>
-                        </div>
-                        <div className="columns section">
-                            <div className="column">
-                                <h2 className="is-sub-title highlight is-bold">Certifications:</h2>
-                                <p>Growth-Driven Design Agency Certified <span className="regular-font">(Hubspot)</span></p>
-                                <p>Certified Ecommerce Marketing Specialist <span className="regular-font">(DigitalMarketer)</span></p>
-                                <p>Social Media Marketing <span className="regular-font">(CDP)</span></p>
-                                <p>Social Media Marketing & Online Reputation Management <span className="regular-font">(CDP)</span></p>
-                            </div>
-                            <div className="column ">
-                                <h2 className="is-sub-title highlight is-bold">Skills:</h2>
-                                <p>Research  & Strategy</p>
-                                <p>Visual Identity Systems</p>
-                                <p>User  Experience Design</p>
-                                <p>User Interface Design</p>
-                                <p>Digital Product Design</p>
-                                <p>Brand Development</p>
-                                <p>Graphic Design</p>
-                                <p>Copywriting</p>
-                            </div>
-                            <div className="column ">
-                                <h2 className="is-sub-title highlight is-bold">Talks:</h2>
-                                <p>How to design product pages that get you more sales  <span className="regular-font">(Shopify Meetup PR 2017)</span></p>
-                                <p>Starting at the End  <span className="regular-font">(Google Developers Group PR - Women Techmakers 2017)</span></p>
-                                <p>Accessibility for Web Designers and Developers <span className="regular-font">(Google Developers Group PR - DevFest 2016)</span></p>
-
-                                <div className="columns p-t-35">
-                                    <div className="column ">
-                                        <h2 className="is-sub-title highlight is-bold">Contact:</h2>
-                                        <p className={"clipboard is-size-5-mobile " + (this.state.clipboard ? "is-tooltip-top  tooltip is-tooltip-active" : "")} data-tooltip="Email copyed to clipboard!"  onClick={this.activateLasers.bind(this)} data-clipboard-text="zoraida.salcedo@outlook.com">zoraida.salcedo@outlook.com</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                <div className="column is-full">
+                  <h2 className="highlight is-sub-title is-bold">What I do</h2>
+                  <p>
+                    I've done many things in my life, design just happens to be
+                    my vocation.{" "}
+                    <span className="highlight">
+                      I want to keep challenging my self with awesome work that
+                      provides me the opportunity to positively affect people.
+                    </span>
+                  </p>
                 </div>
-                </section>
+                <div className="column is-full">
+                  <h2 className="highlight is-sub-title is-bold">
+                    Little details
+                  </h2>
+                  <p>
+                    I am very enthusiastic about learning new things. I'm a
+                    history buff, a video game geek, an aspiring peripatetic who
+                    has had the privilege to live in: Puerto Rico, Mexico, and
+                    currently: <span className="highlight"> Dayton, Ohio</span>.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="column is-hidden-movil section">
+              <img
+                src="https://s3.us-east-2.amazonaws.com/porfolio-bucket/zory-porfolio/images/About-Photos.png"
+                alt=""
+              />
+            </div>
+          </div>
+        </header>
+        <section>
+          <div className="columns">
+            <div className="column">
+              <div className="columns">
+                <div className="column">
+                  <h2 className="is-sub-title highlight is-bold has-text-centered">
+                    A little more about me:{" "}
+                  </h2>
+                </div>
+              </div>
+              <div className="columns section">
+                <div className="column ">
+                  <div className="columns is-multiline">
+                  <div className="column is-full">
+                      <h2 className="is-sub-title highlight is-bold">
+                      Education:
+                      </h2>
+                      <p>
+                      BFA - Digital Art and Design | Motion Minor <span className="regular-font">(EAPD)</span>
+                      </p>
+                    </div>
+                    <div className="column is-full">
+                      <h2 className="is-sub-title highlight is-bold">
+                        Certifications:
+                      </h2>
+                      <p>
+                        Growth-Driven Design Agency Certified{" "}
+                        <span className="regular-font">(Hubspot)</span>
+                      </p>
+                      <p>
+                        Certified Ecommerce Marketing Specialist{" "}
+                        <span className="regular-font">(DigitalMarketer)</span>
+                      </p>
+                      <p>
+                        Social Media Marketing{" "}
+                        <span className="regular-font">(CDP)</span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="column ">
+                  <h2 className="is-sub-title highlight is-bold">Skills:</h2>
+                  <p>Research & Strategy</p>
+                  <p>User Experience Design</p>
+                  <p>User Interface Design</p>
+                  <p>Digital Product Design</p>
+                  <p>Visual Identity Systems</p>
+                  <p>Brand Development</p>
+                  <p>Graphic Design</p>
+                  <p>Copywriting</p>
+                </div>
+                <div className="column ">
+                  <h2 className="is-sub-title highlight is-bold">Talks:</h2>
+                  <p>
+                    How to design product pages that get you more sales{" "}
+                    <span className="regular-font">
+                      (Shopify Meetup PR 2017)
+                    </span>
+                  </p>
+                  <p>
+                    Starting at the End{" "}
+                    <span className="regular-font">
+                      (Google Developers Group PR - Women Techmakers 2017)
+                    </span>
+                  </p>
+                  <p>
+                    Accessibility for Web Designers and Developers{" "}
+                    <span className="regular-font">
+                      (Google Developers Group PR - DevFest 2016)
+                    </span>
+                  </p>
 
-            </section>
-        ) 
-
-     }
+                  <div className="columns p-t-35">
+                    <div className="column ">
+                      <h2 className="is-sub-title highlight is-bold">
+                        Contact:
+                      </h2>
+                      <p
+                        className={
+                          "clipboard is-size-5-mobile " +
+                          (this.state.clipboard
+                            ? "is-tooltip-top  tooltip is-tooltip-active"
+                            : "")
+                        }
+                        data-tooltip="Email copyed to clipboard!"
+                        onClick={this.activateLasers.bind(this)}
+                        data-clipboard-text="zoraida.salcedo@outlook.com"
+                      >
+                        zoraida.salcedo@outlook.com
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </section>
+    );
+  }
 }
 
-
-export default About
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,97 +1,164 @@
-import Link from 'next/link'
-import ClipboardJS from 'clipboard/dist/clipboard'
-import Layout from '../components/Layout'
+import Link from "next/link";
+import ClipboardJS from "clipboard/dist/clipboard";
+import Layout from "../components/Layout";
 
-
-
-class Index extends React.Component  {
-    state = {
-        clipboard: false
-    }
-    activateLasers = () => {
-        new ClipboardJS('.clipboard');
-        this.setState({clipboard: true });
-        setTimeout(() => {
-            this.setState({clipboard: false });
-        }, 1000)
-    }
-    render(){
-        return(
-                <section className="section">
-                    <header className="m-b-150">
-                    <h1 className="is-main-title m-b-20"> 
-                            Hi, I’m a <span className="highlight">Visual &#38; Brand Designer</span> focused on user interface and experience design.
-                    </h1>
-                    <h1 className="is-main-title">
-                        Take a look around, drop me <span className={"is-black-link is-bold  clipboard " + (this.state.clipboard ? "is-tooltip-top  tooltip is-tooltip-active" : "")} data-tooltip="Email copyed to clipboard!"  onClick={this.activateLasers.bind(this)} data-clipboard-text="zoraida.salcedo@outlook.com"><u>a line</u></span>, check my Linked<a href="https://www.linkedin.com/in/zoraidasalcedo" className="is-black-link is-aileron-bold" target="_black"><u>in</u></a> or download my <a href="/static/ZoraidaSalcedo-CV.pdf" target="_blank" className="is-black-link is-bold"><u>CV</u></a>.
-                    </h1>
-                    </header> 
-                    
-                    <div className="columns is-multiline">
-                    
-                        <div className="column is-half">
-                            <div className="grid">
-                                <figure className="effect-sadie">
-                                    <img src="/static/assets/img/Home-ThinkTelic-Image.jpg" alt="Home-ThinkTelic-Image"/>
-                                    <figcaption>
-                                        <h2 className="highlight">Think Telic</h2>
-                                        <p>Brand Identity &#38; UX/UI Design</p>
-                                        <Link href="/project/think-telic">
-                                            <a>View more about Think Telic</a> 
-                                        </Link>
-                                    </figcaption>			
-                                </figure>
+class Index extends React.Component {
+  state = {
+    clipboard: false
+  };
+  activateLasers = () => {
+    new ClipboardJS(".clipboard");
+    this.setState({ clipboard: true });
+    setTimeout(() => {
+      this.setState({ clipboard: false });
+    }, 1000);
+  };
+  render() {
+    return (
+      <section className="section">
+        <header className="m-b-150">
+          <h1 className="is-main-title m-b-20">
+            Hi, I’m a{" "}
+            <span className="highlight">Visual &#38; Brand Designer</span>{" "}
+            focused on user interface and experience design.
+          </h1>
+          <h1 className="is-main-title">
+            Take a look around, drop me{" "}
+            <span
+              className={
+                "is-black-link is-bold  clipboard " +
+                (this.state.clipboard
+                  ? "is-tooltip-top  tooltip is-tooltip-active"
+                  : "")
+              }
+              data-tooltip="Email copyed to clipboard!"
+              onClick={this.activateLasers.bind(this)}
+              data-clipboard-text="zoraida.salcedo@outlook.com"
+            >
+              <u>a line</u>
+            </span>
+            , check my Linked
+            <a
+              href="https://www.linkedin.com/in/zoraidasalcedo"
+              className="is-black-link is-aileron-bold"
+              target="_black"
+            >
+              <u>in</u>
+            </a>{" "}
+            or download my{" "}
+            <a
+              href="/static/ZoraidaSalcedo-CV.pdf"
+              target="_blank"
+              className="is-black-link is-bold"
+            >
+              <u>CV</u>
+            </a>
+            .
+          </h1>
+        </header>
+        <div className="columns">
+          <div className="column ">
+            <div className="columns is-multiline">
+              <div className="column is-full">
+                <h2 className="is-size-1 m-b-20">How I can help.</h2>
+                <p>
+                  I’ve worked with startups and small teams helping define users
+                  goals, developing user experiences, and crafting suitable user
+                  interfaces so that brands can grow.{" "}
+                  <span className="highlight">
+                    I’m currently looking for new opportunities and am available
+                    for hire.
+                  </span>{" "}
+                  You can find a selection of projects below.
+                </p>
+              </div>
+            </div>
+            <div className="columns is-multiline ">
+              <div className="column">
+                <i>
+                  <img src="https://s3.us-east-2.amazonaws.com/porfolio-bucket/zory-porfolio/images/UX-Design-Icon.svg" />
+                </i>
+                <h3>UX Design</h3>
+                <p className="is-size-7">
+                  My focus is to understand your business goals and validate the
+                  problem with your users' needs in mind.
+                </p>
+              </div>
+              <div className="column">
+                <i>
+                  <img src="https://s3.us-east-2.amazonaws.com/porfolio-bucket/zory-porfolio/images/UX-Design-Icon.svg" />
+                </i>
+                <h3>UI Design</h3>
+                <p className="is-size-7">
+                  I structure the design of your product by developing
+                  wireframes and prototypes, and defining visual aesthetics.
+                </p>
+              </div>
+              <div className="column">
+                <i>
+                  <img src="https://s3.us-east-2.amazonaws.com/porfolio-bucket/zory-porfolio/images/UX-Copy-Icon.svg" />
+                </i>
+                <h3>UX Writing</h3>
+                <p className="is-size-7">
+                  I create site maps, content analysis, and write persuasive
+                  copy conveying the message you want to send.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="column is-hidden-movil section">
+            <img
+              src="https://s3.us-east-2.amazonaws.com/porfolio-bucket/zory-porfolio/images/Image-Home.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="columns is-multiline section">
+          <div className="column is-full">
+            <div className="card highlight-border">
+              <div className="columns">
+                <div className="column">
+                  <img
+                    src="https://s3.us-east-2.amazonaws.com/porfolio-bucket/zory-porfolio/images/st/ST-Home.jpg"
+                    alt=""
+                  />
+                </div>
+                <div className="column ">
+                  <div className="columns is-multiline">
+                    <div className="column">
+                      <div class="card-content">
+                        <div className="content">
+                          <span className="is-size-3 highlight">StayTuned</span>
+                          <h3 className="is-size-3 title-font">
+                            Online Management Software
+                          </h3>
+                          <p>
+                            I led the design of this dual dashboard that
+                            provides managers with the tools they need to manage
+                            employees and provides a better customer experience
+                            that provides easier collaboration.
+                          </p>
+                          <div className="columns is-multiline section p-100 ">
+                            <div className="column"> <a class="button is-danger">read full case study</a></div>
+                            <div className="column">
+                              {" "}
+                              <p className="is-size-7">
+                                UX | UI | Product Design{" "}
+                              </p>
                             </div>
+                          </div>
                         </div>
-                        <div className="column is-half">
-                            <div className="grid">
-                                <figure className="effect-sadie">
-                                    <img src="/static/assets/img/Home-Hernandez2-Image.jpg" alt="Home-ThinkTelic-Image"/>
-                                    <figcaption>
-                                        <h2 className="highlight">Hernandez2</h2>
-                                        <p>Personal Brand & UX/UI Design</p>
-                                        <Link href="/project/hernandez2">
-                                            <a>View more about Hernandez2 </a> 
-                                        </Link>
-                                    </figcaption>			
-                                </figure>
-                            </div>
-                        </div>
-                        <div className="column is-half">
-                            <div className="grid">
-                                <figure className="effect-sadie">
-                                    <img src="/static/assets/img/Home-Skills-Image.jpg" alt="Home-ThinkTelic-Image"/>
-                                    <figcaption>
-                                        <h2 className="highlight">Skills Creative Studios</h2>
-                                        <p>UX/UI Design</p>
-                                        <Link href="/project/skills-creative-studios">
-                                            <a>View more about Skills Creative Studios </a> 
-                                        </Link>
-                                    </figcaption>			
-                                </figure>
-                            </div>
-                        </div>
-                        <div className="column is-half">
-                            <div className="grid">
-                                <figure className="effect-sadie">
-                                    <img src="/static/assets/img/Home-collection.jpeg" alt="Home-ThinkTelic-Image"/>
-                                    <figcaption>
-                                        <h2 className="highlight">Identities &#38; Designs</h2>
-                                        <p>A collection of various design projects</p>
-                                        <Link href="/project/collection-projects">
-                                            <a>View more about Skills Creative Studios </a> 
-                                        </Link>
-                                    </figcaption>			
-                                </figure>
-                            </div>
-                        </div>
+                      </div>
                     </div>
-                </section>
-        )
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
 
-    }
-
-} 
-
-
-export default Index
+export default Index;
